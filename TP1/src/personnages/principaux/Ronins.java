2f673219f9a624b93f2de735c761208c91d2f4c0
anais <anais@Ana>
2025-10-19 18:07:03 +0200

    Sauvegarde

--- TP1/src/personnages/principaux/Ronins.java
+++ TP1/src/personnages/principaux/Ronins.java
@@ -9,4 +9,22 @@ public class Ronins extends Humains{
 		this.honneur=1;
 	}
 	
+	public void donner(int n, Commercants c) {
+		c.recevoir(n);
+	}
+	
+	public void provoquer(Yakuzas y) {
+		if(this.honneur*2>y.getReputation()) {
+			this.gagnerArgent(y.getArgent());
+			this.honneur++;
+			y.perdreDuel();
+			this.parler("J'ai gagné le Duel!");
+		}
+		else {
+			this.honneur--;
+			this.parler("J'ai perdus mon duel...");
+			y.gagnerDuel();
+		}
+	}
+	
 }
